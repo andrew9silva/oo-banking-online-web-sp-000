2cd86f8f655126912b890c7973451b6c513a79f7
Andrew Silva <andrewsilva9@gmail.com>
2019-05-17 02:58:15 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,5 +22,10 @@ class Transfer
       @sender.balance -= @amount
       receiver.balance += @amount
       @status = "complete"
+    else
+      status = "rejected"
+      "Transaction rejected. Please chack your account balance"
+    end
+  end
 
 end
