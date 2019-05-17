50c2283ecc57a2936b5c450c65b202a718fdbce5
Andrew Silva <andrewsilva9@gmail.com>
2019-05-17 02:27:49 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,6 @@
 class Transfer
-  attr_accessor :sender, :receiver, :transfer_amount
-  attr_reader :status
+  attr_accessor :sender, :receiver, :transfer_amount, :status
+  
 
   def initialize
     @sender = sender
