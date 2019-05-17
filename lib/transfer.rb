81c9ce339170265323dc046937a23650d9471f5b
Andrew Silva <andrewsilva9@gmail.com>
2019-05-17 02:25:56 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,11 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :transfer_amount
+  attr_reader :status
+
+  def initialize
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @transfer_amount = amount
+  
 end
